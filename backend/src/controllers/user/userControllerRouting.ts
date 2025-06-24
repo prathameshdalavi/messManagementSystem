16950058d1d8ea42e2868f231b57404ef75a3b66
Prathamesh Dalavi <prathameshdalavi04@gmail.com>
2025-06-24 19:46:01 +0530

    added menu service for user

--- backend/src/controllers/user/userControllerRouting.ts
+++ backend/src/controllers/user/userControllerRouting.ts
@@ -1,10 +1,10 @@
 import { Router } from "express";
 import authRouter from "./authController";
 import messRouter from "./messController";
-
+import menuRouter from "./menuController";
 const userRouter = Router();
 
 userRouter.use("/auth", authRouter);
 userRouter.use("/messes", messRouter);
-
+userRouter.use("/menu", menuRouter); 
 export default userRouter;
