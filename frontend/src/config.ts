ce57ff37fbfc3d7b21b5cf189c098ea4c0186311
Prathamesh Dalavi <prathameshdalavi04@gmail.com>
2025-09-23 01:21:12 +0530

    update backend url

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,4 +1,1 @@
-export const BACKEND_URL =
-  process.env.NODE_ENV === "development"
-	? "http://localhost:3000"
-	: "https://messmanagementsystem.onrender.com/";
+export const BACKEND_URL = "https://messmanagementsystem.onrender.com";
